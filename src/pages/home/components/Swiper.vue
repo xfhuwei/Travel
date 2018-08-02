9768c0b50b49309eb9a235d2083c1a89f1afec77
laohu <635888501@qq.com>
2018-08-02 21:47:26 +0800

    首页轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png'
+        }
+      ]
+    }
+  }
+  // computed: {
+  //   swiper () {
+  //     return this.$refs.mySwiper.swiper
+  //   }
+  // },
+  // mounted () {
+  //   // current swiper instance
+  //   // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
+  //   console.log('this is current swiper instance object', this.swiper)
+  //   this.swiper.slideTo(3, 1000, false)
+  // },
+  // methods: {
+  //   callback () {
+
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active // 样式穿透
+    background #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 24.5333%
+    background #ccc
+    .swiper-img
+      width 100%
+
+</style>
