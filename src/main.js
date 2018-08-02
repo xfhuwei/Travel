32cdd325732e13a0197be4042edaf4552b298022
laohu <635888501@qq.com>
2018-08-02 18:00:14 +0800

    首页头部

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastClick' // 移动端点击300ms延迟问题解决方案
-import './assets/styles/reset.css' // 初始化css
-import './assets/styles/border.css' // 1px边框
+import 'styles/reset.css' // 初始化css
+import 'styles/border.css' // 1px边框
+import 'styles/iconfont.css' // 1px边框
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
