ecf133af4bfe66041770c5f4fc69570735608f56
laohu <635888501@qq.com>
2018-12-27 20:55:17 +0800

    'keep-alive'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,12 +27,16 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       mainData: ''
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -43,7 +48,14 @@ export default {
     }
   },
   mounted () { // 钩子，在 dom 渲染完成后执行
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () { // 钩子，配合 keep-alive 缓存标签使用
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
